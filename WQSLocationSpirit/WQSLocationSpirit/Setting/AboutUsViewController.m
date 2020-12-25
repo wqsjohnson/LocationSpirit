8f8df6b8d5fa76c5ec8f016806ffd6cca19f3865
wqsjohnson <498393514@qq.com>
2020-12-25 17:41:21 +0800

    关于页面修改

--- WQSLocationSpirit/WQSLocationSpirit/Setting/AboutUsViewController.m
+++ WQSLocationSpirit/WQSLocationSpirit/Setting/AboutUsViewController.m
@@ -50,6 +50,25 @@
     [backBtn addTarget:self
                 action:@selector(backAcvtion:)
       forControlEvents:UIControlEventTouchUpInside];
+    
+    UIImageView *iconImageView = UIImageView.new;
+    iconImageView.image = [UIImage imageNamed:@"AppIcon"];
+    [self.view addSubview:iconImageView];
+    [iconImageView mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.centerX.mas_equalTo(self.view);
+        make.top.mas_equalTo(self.view).offset(kNavigationBarHeight + 60);
+        make.size.mas_equalTo(CGSizeMake(60, 60));
+    }];
+    
+    UILabel *versionLabel = UILabel.new;
+    versionLabel.text = @"版本: 1.0";
+    versionLabel.textColor = [UIColor blackColor];
+    versionLabel.font = [UIFont systemFontOfSize:15];
+    [self.view addSubview:versionLabel];
+    [versionLabel mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.centerX.mas_equalTo(self.view);
+        make.top.mas_equalTo(iconImageView.mas_bottom).offset(10);
+    }];
 }
 
 - (void)backAcvtion:(UIButton *)sender {
